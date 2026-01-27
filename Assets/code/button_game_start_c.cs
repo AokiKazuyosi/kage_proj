53db9eb88afb7b5655f145de287c302d25b09c96
yasumiti <shoking0821@icloud.com>
2026-01-27 12:20:41 +0900

    SE

--- Assets/code/button_game_start_c.cs
+++ Assets/code/button_game_start_c.cs
@@ -3,37 +3,49 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class button_game_start_c : MonoBehaviour
+public class button_game_start_c : MonoBehaviour, IPointerEnterHandler
 {
     public Button m_startBtn;
-
 
     public Image buttonImage;
     public TextMeshProUGUI buttonText;
 
+    [Header("SE")]
+    public AudioClip hoverSE;   // カーソル音
+    public AudioClip clickSE;   // クリック音
+
+    AudioSource audioSource;
+
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+
         m_startBtn.onClick.AddListener(osu);
-
-        //GetComponent<Button>().onClick.AddListener(osu);
-
-
         StartCoroutine(Blink());
     }
 
     private void OnDestroy()
     {
-
         m_startBtn.onClick.RemoveListener(osu);
-
     }
 
+    // クリック時
     void osu()
     {
         StopAllCoroutines();
-        SetAlpha(1f); // 押した瞬間に表示固定
+        SetAlpha(1f);
+
+        audioSource.PlayOneShot(clickSE);
+
         SceneManager.LoadScene("DifficultyScene");
+    }
+
+    // カーソルが乗った瞬間
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(hoverSE);
     }
 
     IEnumerator Blink()
